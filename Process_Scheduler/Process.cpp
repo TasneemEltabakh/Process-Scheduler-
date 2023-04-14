b85b33bf6c80d7dc980db47d71fac859b6a8f655
Ariam Medhat <202100489@zewailcity.edu.eg>
2023-04-15 00:14:46 +0200

    PROCESS

--- Process_Scheduler/Process.cpp
+++ Process_Scheduler/Process.cpp
@@ -1,1 +1,95 @@
-#include "Process.h"
+#include "Process.h"\
+
+Process::Process() {
+	
+}
+
+Process::~Process() {
+
+}
+
+int Process::getPID() const {
+	return PID;
+}
+
+int Process::getAT()const {
+	return AT;
+}
+
+int Process::getCT() const {
+	return CT;
+}
+
+int Process::getWT() const {
+	return WT;
+}
+
+int Process::getRT() const {
+	return RT;
+}
+
+int Process::getTRT() const {
+	return TRT;
+}
+
+int Process::getTT() const {
+	return TT;
+}
+
+int Process::getKT() const {
+	return KT;
+}
+
+int Process::getIO() const {
+	return IO;
+}
+
+void Process::setPID(int x) {
+	PID = x;
+}
+
+void Process::setAT(int x) {
+	AT = x;
+}
+
+void Process::setKT(int x) {
+	KT = x;
+}
+
+void Process::setIO(int x) {
+	IO = x;
+}
+
+void Process::setCT(int x) {
+	CT = x;
+}
+
+void Process::calcWT() {
+	WT = TRT - CT;
+}
+
+void Process::setRT(int x) {
+	RT = x;
+}
+
+void Process::calcTRT() {
+	TRT = TT - AT;
+}
+
+void Process::setTT(int x) {
+	TT = x;
+}
+
+bool Process::RRmigration(int RTF) {
+	if (RT < RTF)
+		return true;
+	else
+		return false;
+}
+
+bool Process::FCFSmigration(int MaxW) {
+	if (WT > MaxW)
+		return true;
+	else
+		return false;
+}
--- Process_Scheduler/Process.h
+++ Process_Scheduler/Process.h
@@ -1,5 +1,46 @@
 #pragma once
 class Process
+
 {
+private:
+	Process* IO ;
+	int PID, TT, AT, CT, WT, RT, TRT, IO, IO_R, IO_D, KT;
+
+	enum state {
+		NEW,
+		RDY,
+		RUN,
+		BLK,
+		TRM,
+		ORPH
+	};
+	static int count ;
+
+public:
+	Process();
+	~Process();
+	int getPID() const;
+	int getAT()const;
+	int getCT() const;
+	int getWT() const;
+	int getRT() const;
+	int getTRT() const;
+	int getTT() const;
+	int getIO() const;
+	int getKT() const;
+
+	void setPID(int x);
+	void setAT(int x);
+	void setCT(int x);
+	void calcWT();
+	void setRT(int x);
+	void calcTRT();
+	void setTT(int x);
+	void setKT(int x);
+	void setIO(int x);
+
+	bool RRmigration(int RTF);
+	bool FCFSmigration(int MaxW);
+
 };
 
